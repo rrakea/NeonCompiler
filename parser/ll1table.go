7a4f4d2a6879aa4cec45f2419736378427288291
KonradBu <konradburgi1@gmail.com>
2024-11-20 19:27:00 +0100

    ll 1 table

---
+++ parser/ll1table.go
@@ -0,0 +1,21 @@
+package parser
+
+type parsetable struct{
+
+}
+
+func (table *parsetable) string getStartSymbol(){
+
+}
+
+func (table *parsetable) string[] getNonTerminals(){
+
+}
+
+func (table *parsetable) string[] getTerminals(){
+
+}
+
+func (table *parsetable) string getEpsilon(){
+
+}
