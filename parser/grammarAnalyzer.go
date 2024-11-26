64f6052b7f0993567f64eaa08d32a65186799ed5
KonradBu <konradburgi1@gmail.com>
2024-11-26 13:10:02 +0100

    grammarAnalyzer start

--- parser/grammarAnalyzer.go
+++ parser/grammarAnalyzer.go
@@ -1,21 +1,53 @@
 package parser
 
-func (grammar *Grammar) getNullable() []string{
+func (grammar *Grammar) GetNullable() []string{
 	var nullable []string
+	nullableMap := make(map[string]bool)
+	grammar.recursiveNullable(nullableMap, nullable)
+	return nullable
+}
+
+func (grammar *Grammar) recursiveNullable(nullableMap map[string]bool, nullable []string){
+	changed := false
 	for _, rule := range grammar.rules{
 		// Ist die Regel eine Epsilon Transition?
 		if rule.production[0] == " "{
 			nullable = append(nullable, rule.nonTerminal)
+			nullableMap[rule.nonTerminal] = true
+			changed = true
+		}else{
+			for _, s := range rule.production{
+				if nullableMap[s]{
+					nullable = append(nullable, rule.nonTerminal)
+					nullableMap[rule.nonTerminal] = true
+					changed = true
+				}
+			}
 		}
 	}
-	
-	return nullable
+	if changed{
+		grammar.recursiveNullable(nullableMap, nullable)
+	}
 }
 
-func (grammar *Grammar) getFirst() map[string][]string{
+func (grammar *Grammar)recursiveSearch(inputMap map[string]any,closure func(*Grammar)()(bool)){
 
 }
 
-func (grammar *Grammar) getFollow()map[string][]string{
+func (grammar *Grammar) GetFirst(nullable []string) map[string][]string{
+	var resMap map[string][]string
+	for _, nt := range grammar.nonTerminals{
+		var first []string
+		var firstMap map[string]bool
+		grammar.recursiveFirst(nt, first, firstMap)
+		resMap[nt] = first
+	}
+}
+
+func (grammar *Grammar)recursiveFirst(nt string, first []string, firstMap map[string]bool){
+
+}
+
+func (grammar *Grammar) GetFollow()map[string][]string{
 
 }
