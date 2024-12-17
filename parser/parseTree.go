1609516454b3221119158e3cab674ee279fb0897
rakeaDev <122118878+KonradBu@users.noreply.github.com>
2024-12-17 19:11:24 +0100

    Output tree

--- parser/parseTree.go
+++ parser/parseTree.go
@@ -2,25 +2,26 @@ package parser
 
 import (
 	"compiler/lexer"
-	"fmt"
-	"github.com/m1gwings/treedrawer/tree"
+	"slices"
+
+	"github.com/pterm/pterm"
 )
 
 type parseTree struct {
-	leaf parseLeaf
+	leaf     parseLeaf
 	branches []parseTree
 }
 
-type parseLeaf struct{
-	name string
+type parseLeaf struct {
+	name  string
 	value any
-} 
+}
 
-func createParseTree(parseChan chan any)  {
+func createParseTree(parseChan chan any) {
 	Trees := []parseTree{}
-	for true{
-		newItem := <- parseChan 
-		switch newItem.(type){
+	for true {
+		newItem := <-parseChan
+		switch newItem.(type) {
 		case lexer.Token:
 			token := newItem.(lexer.Token)
 			newLeaf := parseLeaf{name: token.Identifier, value: token.Value}
@@ -31,15 +32,16 @@ func createParseTree(parseChan chan any)  {
 			newTree := parseTree{}
 			newBranches := []parseTree{}
 			newTree.leaf = parseLeaf{name: rule.nonTerminal, value: 0}
-			for i := range rule.production{
+			for i := range rule.production {
 				len := len(Trees)
-				if  len == 0{
+				if len == 0 {
 					panic("Parse Tree Error, no new node possible")
 				}
-				
-				newBranches = append(newBranches, Trees[len - i -1])
+
+				newBranches = append(newBranches, Trees[len-i-1])
 			}
-			Trees = Trees[:len(Trees) - len(rule.production)]
+			Trees = Trees[:len(Trees)-len(rule.production)]
+			slices.Reverse(newBranches)
 			newTree.branches = newBranches
 			Trees = append(Trees, newTree)
 		case bool:
@@ -51,13 +53,16 @@ func createParseTree(parseChan chan any)  {
 	close(parseChan)
 }
 
-func PrintTree(tree parseTree){
-	fmt.Print(tree.leaf.name)
-	fmt.Print(" (")
-	fmt.Print(tree.leaf.value)
-	fmt.Print("): ")
-	fmt.Print(tree.branches)
-	for _, t := range tree.branches{
-		PrintTree(t)
+func PrintTree(tree parseTree) {
+	ptree := makePTree(tree)
+	renderTree := pterm.DefaultTree.WithRoot(ptree)
+	renderTree.Render()
+}
+
+func makePTree(tree parseTree) pterm.TreeNode {
+	root := pterm.TreeNode{Text: tree.leaf.name, Children: []pterm.TreeNode{}}
+	for _, t := range tree.branches {
+		root.Children = append(root.Children, makePTree(t))
 	}
+	return root
 }
---
+++ testcode/Test.cs
@@ -0,0 +1,8 @@
+using System;
+namespace test {
+    class test{
+        static void main (string[] args) {
+            val = 1 + 2 * 3;
+        } 
+    }
+}
