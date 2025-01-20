eabe028fb784ad054f1642fbd446c4fcf834fa5b
KonradBu <konradburgi1@gmail.com>
2025-01-20 14:19:28 +0100

    smoll

--- testcode/Test.cs
+++ testcode/Test.cs
@@ -1,8 +1,8 @@
 using System;
 namespace test {
     class test{
-        static void main (string[] args) {
-            val = 1 + 2 * 3;
+        static void Main (string[] args) {
+            int val = 1 + 2 * 3;
         } 
     }
 }
