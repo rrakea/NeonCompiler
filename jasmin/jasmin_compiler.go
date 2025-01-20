384c73d892264ae980de45b247dc6aa66b196d74
KonradBu <konradburgi1@gmail.com>
2025-01-20 14:53:39 +0100

    fix 2/25

--- jasmin/jasmin_compiler.go
+++ jasmin/jasmin_compiler.go
@@ -102,7 +102,7 @@ func Build_jasmin(parsetree *tree, info *typechecker.TypeCheckerInfo, file_name 
 		local_var_code := ""
 		for var_index, local_var := range info.LocalVar[function.Name] {
 			ex_code, ex_type, ex_stack_limit, ex_locals_used := expression_evaluation(&local_var.Expression, &var_info, build, &func_sigs, &labels)
-			if ex_type != local_var.Vartype {
+			if ex_type != jasmin_type_converter(local_var.Vartype) {
 				panic("Internal Error: Type Checked Expression does not equal actual type of expression")
 			}
 			// Set which local vars were used in the expression
--- testcode/Test.cs.j
+++ testcode/Test.cs.j
@@ -1,4 +1,4 @@
-.class <public> <testcode/Test.cs>
+.class <public> </home/rrakea/Code/Uni/NeonCompiler/testcode/Test.cs>
 .super <java/lang/object> 
 .method static <clinit>()V 
 .limit locals 0.limit stack 0return 
